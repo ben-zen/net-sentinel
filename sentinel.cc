34b0690cbac631e5dfa14364461ef8c76181182e
Ben Lewis <1884340+ben-zen@users.noreply.github.com>
2021-11-27 22:50:44 -0800

    Built a very straightforward echo request, not yet reading the response.

--- sentinel.cc
+++ sentinel.cc
@@ -5,13 +5,39 @@
 #include <ifaddrs.h>
 #include <net/if.h>
 #include <netinet/in.h>
+#include <netinet/ip.h>
+#include <netinet/ip_icmp.h>
 #include <netdb.h>
 #include <memory>
 #include <optional>
 #include <stdio.h>
 #include <string.h>
 #include <sys/ioctl.h>
+#include <sys/time.h>
 #include <unistd.h>
+
+class socket_t {
+public:
+  socket_t(int domain, int type, int protocol) {
+    _fd = socket(domain, type, protocol);
+    if (_fd <= 0) {
+      perror("Failed opening a socket.");
+    }
+  }
+  ~socket_t() {
+    if (_fd > 0) {
+      puts("Closing socket.");
+      close(_fd);
+    }
+  }
+
+  operator int() {
+     return _fd;
+  }
+
+private:
+  int _fd;
+};
 
 struct Ifconf : ifconf {
   Ifconf() {
@@ -68,7 +94,10 @@ std::optional<sockaddr> find_interface(int socket_fd, const char *if_name, sa_fa
           return {};
         }
         printf("\taddress: <%s>\n", host);
-        return {*(cursor->ifa_addr)};
+        sockaddr addr = {};
+        addr.sa_family = AF_INET;
+        ((sockaddr_in *)(&addr))->sin_addr = ((sockaddr_in *)(cursor->ifa_addr))->sin_addr;
+        return {addr};
       }
     }
   cursor = cursor->ifa_next;
@@ -77,25 +106,91 @@ std::optional<sockaddr> find_interface(int socket_fd, const char *if_name, sa_fa
   return {};
 }
 
+// Adapted from the RFC.
+uint16_t compute_checksum(uint8_t *data, size_t len) {
+  uint32_t acc = 0;
+  size_t count = len;
+  while (count > 1) {
+    acc += * (uint16_t *)data;
+    data += 2;
+    count -= 2;
+  }
+
+  if (count > 0) {
+    acc += *data;
+  }
+
+  while (acc >> 16) {
+    acc = (acc & 0xFFFF) + (acc >> 16);
+  }
+
+  return ~acc;
+}
+
+#pragma pack(push,1)
+struct echo_request {
+  icmphdr header {};
+  uint32_t timestamp;
+  char buffer[64] = {};
+  echo_request() {
+    header.type = ICMP_ECHO;
+    header.un.echo.id = getpid();
+    header.un.echo.sequence = 1;
+    struct timeval val{};
+    gettimeofday(&val, nullptr);
+    timestamp = (val.tv_usec % 8460000) / 100;
+  }
+};
+#pragma pack(pop)
+
 int main(int argc, char *argv[]) {
-    // Find an interface matching the supplied name
-    int socket_fd = 0;
-    socket_fd = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP);
-    if (socket_fd <= 0) {
-        perror("socket failed");
-        return 1;
-    }
+
+  socket_t read_socket(AF_INET, SOCK_RAW, IPPROTO_ICMP);
+  if (read_socket <= 0) {
+    perror("socket failed");
+    return 1;
+  }
 
     if (argc == 1) {
         puts("Insufficient arguments!");
         return 2;
     }
 
-    find_interface(socket_fd, argv[1], AF_INET);
+  // Find an interface matching the supplied name
+  auto addr = find_interface(read_socket, argv[1], AF_INET);
+  if (!addr) {
+    printf("Failed to find interface %s\n", argv[1]);
+    return 3;
+  }
 
-    if (socket_fd) {
-        if (close(socket_fd) < 0) {
-            perror("close failed on socket");
-        }
-    }
+  auto *addr_v4 = (sockaddr_in *)&addr.value();
+  addr_v4->sin_port = IPPROTO_ICMP;
+  uint8_t address[4]{};
+  memcpy(address, &addr_v4->sin_addr.s_addr, sizeof(address));
+  printf("Returned struct: %d %x %d.%d.%d.%d\n", addr_v4->sin_family, addr_v4->sin_port, address[0], address[1], address[2], address[3]);
+
+  // Bind to the interface!
+  if (bind(read_socket, &(addr.value()), sizeof(sockaddr_in)) ==-1) {
+    perror("failed to bind to socket with supplied sockaddr");
+    return 4;
+  }
+
+  puts("Bound to socket! Ready to listen?");
+
+  echo_request request{};
+  char example[] = "test icmp echo impl";
+  memcpy(request.buffer, example, sizeof(example));
+  request.header.checksum = compute_checksum((uint8_t *) &request, sizeof(request));
+
+  sockaddr_in destination{};
+  destination.sin_family = AF_INET;
+  destination.sin_port = IPPORT_ECHO;
+  destination.sin_addr.s_addr = 0x08080808; // 8.8.8.8
+
+  if (sendto(read_socket, &request, sizeof(request), 0, (const sockaddr *)&destination, sizeof(destination)) == -1) {
+    perror("sendto failed.");
+    return 5;
+  }
+
+  return 0;
 }
